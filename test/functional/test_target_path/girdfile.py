27f4f51322601b4fbd6696c6e6989f0b02a7d2dd
Tuukka Ruhanen <tuukka.t.ruhanen@gmail.com>
2023-03-25 13:12:16 +0200

    TEST: Path target without a path dependency

--- test/functional/test_target_path/girdfile.py
+++ test/functional/test_target_path/girdfile.py
@@ -3,15 +3,20 @@ import pathlib
 import gird
 
 path_dep = pathlib.Path("dep")
-path_target = pathlib.Path("target")
+path_target_with_dep = pathlib.Path("target_with_dep")
+path_target_without_dep = pathlib.Path("target_without_dep")
 
 
 def create_dep():
     path_dep.touch()
 
 
-def create_target():
-    path_target.touch()
+def create_target_with_dep():
+    path_target_with_dep.touch()
+
+
+def create_target_without_dep():
+    path_target_without_dep.touch()
 
 
 gird.rule(
@@ -21,7 +26,13 @@ gird.rule(
 
 
 gird.rule(
-    target=path_target,
+    target=path_target_with_dep,
     deps=path_dep,
-    recipe=create_target,
+    recipe=create_target_with_dep,
+)
+
+
+gird.rule(
+    target=path_target_without_dep,
+    recipe=create_target_without_dep,
 )
--- test/functional/test_target_path/test_target_path.py
+++ test/functional/test_target_path/test_target_path.py
@@ -3,18 +3,19 @@ import pathlib
 TEST_DIR = pathlib.Path(__file__).parent
 
 
-def test_target_path(tmp_path, run_rule):
-    """Test that the recipe of a Path target is not run if it's dependency is
-    not updated after the target is created, and that the recipe is run if the
-    dependency is updated.
+def test_target_path_with_dep(tmp_path, run_rule):
+    """Test that for a Path target with a Path dependency,
+    - the recipe is not run if it's dependency is not updated after the target
+      is created, and
+    - the recipe is run if the dependency is updated.
     """
     path_dep = tmp_path / "dep"
-    path_target = tmp_path / "target"
+    path_target = tmp_path / "target_with_dep"
 
     run_rule(
         pytest_tmp_path=tmp_path,
         test_dir=TEST_DIR,
-        rule="target",
+        rule="target_with_dep",
     )
 
     mtime_first = path_target.stat().st_mtime_ns
@@ -22,7 +23,7 @@ def test_target_path(tmp_path, run_rule):
     run_rule(
         pytest_tmp_path=tmp_path,
         test_dir=TEST_DIR,
-        rule="target",
+        rule="target_with_dep",
     )
 
     mtime_second = path_target.stat().st_mtime_ns
@@ -34,9 +35,37 @@ def test_target_path(tmp_path, run_rule):
     run_rule(
         pytest_tmp_path=tmp_path,
         test_dir=TEST_DIR,
-        rule="target",
+        rule="target_with_dep",
     )
 
     mtime_third = path_target.stat().st_mtime_ns
 
     assert mtime_second < mtime_third
+
+
+def test_target_path_without_dep(tmp_path, run_rule):
+    """Test that for a Path target without a path dependency,
+    - the recipe is run when the target doesn't exist, and
+    - the recipe is not run when the target exists.
+    """
+    path_target = tmp_path / "target_without_dep"
+
+    run_rule(
+        pytest_tmp_path=tmp_path,
+        test_dir=TEST_DIR,
+        rule="target_without_dep",
+    )
+
+    assert path_target.exists()
+
+    mtime_first = path_target.stat().st_mtime_ns
+
+    run_rule(
+        pytest_tmp_path=tmp_path,
+        test_dir=TEST_DIR,
+        rule="target_without_dep",
+    )
+
+    mtime_second = path_target.stat().st_mtime_ns
+
+    assert mtime_first == mtime_second
