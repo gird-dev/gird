1543ec55043c6af2ad043df80159025d40cc0822
Tuukka Ruhanen <tuukka.t.ruhanen@gmail.com>
2023-03-26 14:22:29 +0300

    REFACTOR: Remove unneeded code
    - Removed function `get_python_function_shell_command` that has not been needed
      since replacing Make.
    - Increased the version number to 2.0.1.

--- gird/utils.py
+++ gird/utils.py
@@ -1,29 +1,7 @@
 """General utility code"""
 
 import datetime
-import inspect
 import pathlib
-from typing import Callable
-
-
-def get_python_function_shell_command(
-    function: Callable,
-) -> str:
-    """Get a shell command for running a Python function.
-
-    Parameters
-    ----------
-    function
-        The function to call. The module that contains the function must be
-        found in PYTHONPATH.
-    """
-    module = inspect.getmodule(function)
-    module_name = module.__name__
-    function_name = function.__name__
-    shell_command = (
-        f"python -c 'from {module_name} import {function_name}; " f"{function_name}()'"
-    )
-    return shell_command
 
 
 def get_path_modified_time(path: pathlib.Path) -> datetime.datetime:
